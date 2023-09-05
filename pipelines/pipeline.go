067588a2eb57795378ab5b649248359005b71113
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-09-05 16:02:53 +0300

    Add godocs for pipelines

--- pipelines/pipeline.go
+++ pipelines/pipeline.go
@@ -4,12 +4,28 @@ import (
 	"github.com/takahawk/shadownet/common"
 )
 
+// UploadPipeline groups components to transform and then upload data to
+// storage.
 type UploadPipeline interface {
-	AddSteps(components... common.Component) error
+	// AddSteps add components to upload pipeline. The last component should
+	// always be uploader and uploader can be only the last component in
+	// pipeline
+	AddSteps(components ...common.Component) error
+	// Upload runs the whole pipeline for a given data, transforming and then
+	// uploading it to storage.
+	// ShadowNet URL to access the data is returned afterwards
 	Upload(data []byte) (url string, err error)
 }
 
+// DownloadPipeline groups components to transform and then upload data to
+// storage.
 type DownloadPipeline interface {
-	AddSteps(components... common.Component) error
+	// AddSteps add components to download pipeline. The first component should
+	// always be download and it can be only the first component in
+	// pipeline
+	AddSteps(components ...common.Component) error
+	// Download run the whole pipeline downloading data from storage and then
+	// transforming it and returning back. It is supposed to do effectively
+	// the reverse process of what Uploader.Upload have done
 	Download() (data []byte, err error)
 }
--- pipelines/pipeline_impl.go
+++ pipelines/pipeline_impl.go
@@ -5,24 +5,26 @@ import (
 
 	"github.com/takahawk/shadownet/common"
 	"github.com/takahawk/shadownet/downloaders"
+	"github.com/takahawk/shadownet/transformers"
 	"github.com/takahawk/shadownet/uploaders"
 	"github.com/takahawk/shadownet/url"
-	"github.com/takahawk/shadownet/transformers"
 )
 
 type uploadPipeline struct {
-	finalized bool
+	finalized  bool
 	urlHandler url.UrlHandler
-	steps []common.Component
+	steps      []common.Component
 }
 
+// NewUploadPipeline returns new empty upload pipeline
 func NewUploadPipeline() UploadPipeline {
 	return &uploadPipeline{
 		urlHandler: url.NewUrlHandler(),
 	}
 }
 
-func (up *uploadPipeline) AddSteps(components... common.Component) error {
+// AddSteps adds components to upload pipeline
+func (up *uploadPipeline) AddSteps(components ...common.Component) error {
 	// there can be only one uploader and it will always be the last one
 	if up.finalized {
 		return errors.New("can't add another step. There is already an uploader")
@@ -30,21 +32,20 @@ func (up *uploadPipeline) AddSteps(components... common.Component) error {
 
 	for i, component := range components {
 		if _, ok := component.(uploaders.Uploader); ok {
-			if i != len(components) - 1 {
+			if i != len(components)-1 {
 				return errors.New("uploader can be only the last step")
 			} else {
 				up.finalized = true
 			}
 		}
 	}
-	
 
 	up.steps = append(up.steps, components...)
 
 	return nil
 }
 
-
+// Upload runs the whole pipeline to upload given data returning ShadowNet URL
 func (up *uploadPipeline) Upload(data []byte) (url string, err error) {
 	if !up.finalized {
 		return "", errors.New("pipeline should be finalized with uploader")
@@ -61,21 +62,23 @@ func (up *uploadPipeline) Upload(data []byte) (url string, err error) {
 
 		if err != nil {
 			return "", err
-		}		
+		}
 	}
 
 	return up.urlHandler.MakeURL(id, up.steps...)
 }
 
-
 type downloadPipeline struct {
 	steps []common.Component
 }
 
+// NewDownloadPipeline returns new empty download pipeline
 func NewDownloadPipeline() DownloadPipeline {
 	return &downloadPipeline{}
 }
 
+// NewDownloadPipeline constructs from ShadowNet URL new download pipeline
+// that can be used to download and decode data from it
 func NewDownloadPipelineByURL(shadowUrl string) (DownloadPipeline, error) {
 	urlHandler := url.NewUrlHandler()
 	pipeline := NewDownloadPipeline()
@@ -90,7 +93,8 @@ func NewDownloadPipelineByURL(shadowUrl string) (DownloadPipeline, error) {
 	return pipeline, nil
 }
 
-func (dp *downloadPipeline) AddSteps(components... common.Component) error {
+// AddSteps adds components to download pipeline
+func (dp *downloadPipeline) AddSteps(components ...common.Component) error {
 	// there can be only one downloader and it will always be the first one
 	if len(dp.steps) == 0 && len(components) != 0 {
 		if _, ok := components[0].(downloaders.Downloader); !ok {
@@ -105,12 +109,14 @@ func (dp *downloadPipeline) AddSteps(components... common.Component) error {
 			}
 		}
 	}
-	
+
 	dp.steps = append(dp.steps, components...)
 
 	return nil
 }
 
+// Download runs the whole pipeline to retrieve and trasform data returning the
+// final result
 func (dp *downloadPipeline) Download() (data []byte, err error) {
 	if len(dp.steps) == 0 {
 		return nil, errors.New("empty download pipeline")
@@ -127,6 +133,6 @@ func (dp *downloadPipeline) Download() (data []byte, err error) {
 			return nil, err
 		}
 	}
-	
+
 	return data, nil
 }
