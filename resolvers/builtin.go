42b798b148a4ad51c510409ad1d37d4df7e3164a
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-09-12 15:59:02 +0300

    Add godocs for resolvers

--- resolvers/builtin.go
+++ resolvers/builtin.go
@@ -10,41 +10,44 @@ import (
 )
 
 type builtinResolver struct {
-	downloaderDict map[string] func(params... []byte) (downloaders.Downloader, error)
-	transformerDict map[string] func(params... []byte) (transformers.Transformer, error)
-	uploaderDict map[string] func(params... []byte) (uploaders.Uploader, error)
+	downloaderDict  map[string]func(params ...[]byte) (downloaders.Downloader, error)
+	transformerDict map[string]func(params ...[]byte) (transformers.Transformer, error)
+	uploaderDict    map[string]func(params ...[]byte) (uploaders.Uploader, error)
 }
 
+// NewBuiltinResolver returns new built-in resolver that is created to resolve
+// components built-in into ShadowNet directly (TODO: provide list?)
 func NewBuiltinResolver() Resolver {
 	return &builtinResolver{
-		downloaderDict: map[string] func(params... []byte) (downloaders.Downloader, error) {
-			downloaders.PastebinDownloaderName: func(params... []byte) (downloaders.Downloader, error) { 
+		downloaderDict: map[string]func(params ...[]byte) (downloaders.Downloader, error){
+			downloaders.PastebinDownloaderName: func(params ...[]byte) (downloaders.Downloader, error) {
 				return downloaders.NewPastebinDownloaderWithParams(params...)
 			},
 		},
 
-		transformerDict: map[string] func(params... []byte) (transformers.Transformer, error) {
-			transformers.Base64TransformerName: func(params... []byte) (transformers.Transformer, error) {
+		transformerDict: map[string]func(params ...[]byte) (transformers.Transformer, error){
+			transformers.Base64TransformerName: func(params ...[]byte) (transformers.Transformer, error) {
 				if len(params) != 0 {
 					return nil, errors.New("base64 transformer doesn't accept any params")
 				}
 
 				return transformers.NewBase64Transformer(), nil
 			},
-			transformers.AESEncryptorName: func(params... []byte) (transformers.Transformer, error) {
+			transformers.AESEncryptorName: func(params ...[]byte) (transformers.Transformer, error) {
 				return transformers.NewAESEncryptorWithParams(params...)
 			},
 		},
 
-		uploaderDict: map[string] func(params... []byte) (uploaders.Uploader, error) {
-			uploaders.PastebinUploaderName: func(params... []byte) (uploaders.Uploader, error) {
+		uploaderDict: map[string]func(params ...[]byte) (uploaders.Uploader, error){
+			uploaders.PastebinUploaderName: func(params ...[]byte) (uploaders.Uploader, error) {
 				return uploaders.NewPastebinUploaderWithParams(params...)
 			},
 		},
 	}
 }
 
-func (br *builtinResolver) ResolveDownloader(name string, params... []byte) (downloaders.Downloader, error) {
+// ResolveDownloader returns one of built-in downloaders
+func (br *builtinResolver) ResolveDownloader(name string, params ...[]byte) (downloaders.Downloader, error) {
 	if downloaderFactory, ok := br.downloaderDict[name]; ok {
 		return downloaderFactory(params...)
 	}
@@ -52,18 +55,20 @@ func (br *builtinResolver) ResolveDownloader(name string, params... []byte) (dow
 	return nil, errors.New(fmt.Sprintf("there is no built-in downloader with name %s", name))
 }
 
-func (br *builtinResolver) ResolveTransformer(name string, params... []byte) (transformers.Transformer, error) {
+// ResolveTransformer returns one of built-in transformers
+func (br *builtinResolver) ResolveTransformer(name string, params ...[]byte) (transformers.Transformer, error) {
 	if transformerFactory, ok := br.transformerDict[name]; ok {
 		return transformerFactory(params...)
 	}
 
-	return nil, errors. New(fmt.Sprintf("there is no built-in transformer with name %s", name))
+	return nil, errors.New(fmt.Sprintf("there is no built-in transformer with name %s", name))
 }
 
-func (br *builtinResolver) ResolveUploader(name string, params... []byte) (uploaders.Uploader, error) {
+// ResolveUploader returns one of built-in uploaders
+func (br *builtinResolver) ResolveUploader(name string, params ...[]byte) (uploaders.Uploader, error) {
 	if uploaderFactory, ok := br.uploaderDict[name]; ok {
 		return uploaderFactory(params...)
 	}
 
-	return nil, errors. New(fmt.Sprintf("there is no built-in uploader with name %s", name))
+	return nil, errors.New(fmt.Sprintf("there is no built-in uploader with name %s", name))
 }
--- resolvers/resolvers.go
+++ resolvers/resolvers.go
@@ -6,10 +6,15 @@ import (
 	"github.com/takahawk/shadownet/uploaders"
 )
 
+// Resolver can be used to get ShadowNet component given a name, parameters and
+// type of component
 type Resolver interface {
-	ResolveDownloader(name string, params... []byte) (downloaders.Downloader, error)
-	ResolveTransformer(name string, params... []byte) (transformers.Transformer, error)
-	ResolveUploader(name string, params... []byte) (uploaders.Uploader, error)
+	// ResolveDownloader returns Downloader by name and parameters
+	ResolveDownloader(name string, params ...[]byte) (downloaders.Downloader, error)
+	// ResolveDownloader returns Transformer by name and parameters
+	ResolveTransformer(name string, params ...[]byte) (transformers.Transformer, error)
+	// ResolveDownloader returns Uploader by name and parameters
+	ResolveUploader(name string, params ...[]byte) (uploaders.Uploader, error)
 }
 
 // TODO: add plugin and/or socket and/or IPC bridge implementations
